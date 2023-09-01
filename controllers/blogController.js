933a252aa2b5dbb90288240e8bf26b06d85265dd
Krishanu <krishgoswami15@gmail.com>
2023-09-01 18:17:52 +0530

    finished updateBlog endpoint

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -47,11 +47,15 @@ exports.getBlogById = () => {
     }
 }
 
-exports.updateBlogController = () => {
+exports.updateBlogController = async (req, res) => {
     try {
-
+        const { id } = req.params;
+        const { title, description, image } = req.body;
+        const updatedBlog = await blogModel.findByIdAndUpdate(id, { ...req.body }, { new: true });
+        return res.status(200).send({ message: 'sucessfully updated', updatedBlog });
     } catch (error) {
-
+        console.log(error);
+        res.status(500).send({ message: "error updating blog post" });
     }
 }
 
